96a30bdd0f837150565711ede26d9c74c9c571ee
Fronfronfron666 <pelf1504@usherbrooke.ca>
2023-10-26 15:52:50 -0400

    Added Code Real

---
+++ Code Real/main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+from picar import front_wheels
+from picar import back_wheels
+import time
+import picar
+
+
+picar.setup()
+fw = front_wheels.Front_Wheels(db='config')
+bw = back_wheels.Back_Wheels (db=' config')
+def start_bouge():
+    print('Demarrage')
+    fw.turn_straight() # Correspond a 90
+    bw.forward()
+    bw.speed = 70
+    time.sleep(5)
+    fw.turn(0)
+    bw.backward()
+    bw.speed = 20
+    time.sleep(5)
+    fw.turn(180)
+
+    bw.forward()
+    time.sleep(5)
+    fw.turn(90)  # tout droit
+    bw.forward()
+    bw.speed = 70
+    time.sleep(5)
+    bw.stop()
+
+def stop():
+    bw.stop()
+    fw.turn_straight()
+
+if __name__ == '__main__':
+    try:
+        start_bouge()
+    except KeyboardInterrupt:
+        stop()
