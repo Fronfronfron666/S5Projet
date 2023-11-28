bb97b95b653f05d60d976fcf8c79d4c81a9f9e3d
zaccoss <zach.cossette7@gmail.com>
2023-11-28 11:31:42 -0500

    code sauceur

--- Code Real/main.py
+++ Code Real/main.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     try:
         while True:
             mv.turn_wheels(line_follower.get_turn_value(line_follower.get_line_follower_result()))
-
+            time.sleep(5)
         #start_bouge()
         #print('right')
         #mv.turnWheels(90)
