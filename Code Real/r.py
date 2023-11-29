dbd90f9eb21c511a07b96841f46aa850d2eef5a1
zaccoss <zach.cossette7@gmail.com>
2023-11-29 16:35:08 -0500

    code sauceur jour 2

--- Code Real/r.py
+++ Code Real/r.py
@@ -7,6 +7,10 @@ import mouvement as mv
 import ulrason
 import line_follower
 
+picar.setup()
+fw = front_wheels.Front_Wheels(db='config')
+bw = back_wheels.Back_Wheels (db=' config')
+
 def stop():
     bw.stop()
     fw.turn_straight()
