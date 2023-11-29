e3ae9e7ba09753b4aa5a68aeb4089f169a158cda
zaccoss <zach.cossette7@gmail.com>
2023-11-29 12:28:12 -0500

    code sauceur jour 2

--- Code Real/main.py
+++ Code Real/main.py
@@ -41,8 +41,8 @@ if __name__ == '__main__':
         while True:
             print("counter: ", counter)
 
-            mv.turn_wheels(line_follower.get_turn_value(line_follower.get_line_follower_result()))
-            #mv.turn_wheels(0)
+            #mv.turn_wheels(line_follower.get_turn_value(line_follower.get_line_follower_result()))
+            mv.turn_wheels(0)
             mv.accelerate()
             mv.move()
             counter += 1
