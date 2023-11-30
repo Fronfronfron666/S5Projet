985b33bfe3a177b4d1d9a8e2134eaf8e45c03824
zaccoss <zach.cossette7@gmail.com>
2023-11-29 19:45:03 -0500

    code sauceur jour 2

--- Code Real/francis.py
+++ Code Real/francis.py
@@ -55,13 +55,13 @@ if __name__ == '__main__':
                 elif timeSinceDetect <= 1.85:
                     mv.move_frontward()
                     mv.turn_wheels(0)
-                elif timeSinceDetect <= 2.1:
+                elif timeSinceDetect <= 2:
                     line_follower.previous_sensor_state = [False, False, False, False, False]
                     line_follower.previous_sensor_result = [False, False, False, False, False]
                     print("DDD")
                     mv.move_frontward()
                     mv.turn_wheels(-50)
-                elif timeSinceDetect > 2.13:
+                elif timeSinceDetect > 2.1:
                     print("EEE")
                     flag = False
                     timeSinceDetect = 0.0
