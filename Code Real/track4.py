9751d734e32efb1fb19f2447c7e5821c3c6af361
zaccoss <zach.cossette7@gmail.com>
2023-11-30 13:29:14 -0500

    code sauceur jour 2

--- Code Real/track4.py
+++ Code Real/track4.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
         while True:
             counter += 1
             print(counter)
-            if counter <= 48:
+            if counter <= 49:
                 #turn_value = line_follower.get_turn_value(line_follower.get_line_follower_result())
                 mv.turn_wheels(0)
                 mv.accelerate()
