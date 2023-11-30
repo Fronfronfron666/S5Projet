fae14ff725f5e6f58f12c09316bfbfdd72d50fde
zaccoss <zach.cossette7@gmail.com>
2023-11-30 12:43:58 -0500

    code sauceur jour 2

--- Code Real/mouvement.py
+++ Code Real/mouvement.py
@@ -33,13 +33,13 @@ def checkCallibration():
     bw.stop()
 
 def spin_left():
-    bw.left_wheel.backward()
-    bw.right_wheel.forward()
+    bw.left_wheel.stop()
+    bw.right_wheel.backward()
     bw.speed = currentspeed
 
 def spin_right():
-    bw.left_wheel.forward()
-    bw.right_wheel.backward()
+    bw.left_wheel.backward()
+    bw.right_wheel.stop()
     bw.speed = currentspeed
 
 def check_max_and_min_speed():
