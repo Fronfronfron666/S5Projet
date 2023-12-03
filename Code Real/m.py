6a3f636c5abfba9c2743296702a66c54109eb2d3
zaccoss <zach.cossette7@gmail.com>
2023-12-03 16:58:37 -0500

    code sauceur jour 2

--- Code Real/m.py
+++ Code Real/m.py
@@ -51,40 +51,42 @@ def dodge():
 def process_picar(number, q):
     global detection_time, flag
     last_range_value = 50
+    try:
+        while True:
+            if not flag:
+                mv.turn_wheels(line_follower.get_turn_value(line_follower.get_line_follower_result()))
+                mv.accelerate()
+                mv.move_with_spin()
 
-    while True:
-        if not flag:
-            mv.turn_wheels(line_follower.get_turn_value(line_follower.get_line_follower_result()))
-            mv.accelerate()
-            mv.move_with_spin()
+                if q.empty() is False:
+                    last_range_value = q.get()
+                print("last range value", last_range_value)
 
-            if q.empty() is False:
-                last_range_value = q.get()
-            print("last range value", last_range_value)
-
-            if last_range_value <= 3:
-                flag = True
-        else:
-            print("Dodging")
-            dodge()
-
+                if last_range_value <= 3:
+                    flag = True
+            else:
+                print("Dodging")
+                dodge()
+    except KeyboardInterrupt:
+        stop()
 
 def process_sensor_distance(number, q):
-    while True:
-        range_value = us.get_ultrasonic_avoidance()
-        #print("got range:   ", range_value)
-        q.put(range_value)
+    try:
+        while True:
+            range_value = us.get_ultrasonic_avoidance()
+            #print("got range:   ", range_value)
+            q.put(range_value)
+    except KeyboardInterrupt:
+        stop()
 
 
 if __name__ == '__main__':
     q = multiprocessing.Manager().Queue()
     number = 0
-    try:
-        p_picar = multiprocessing.Process(target=process_picar, args=(number, q))
-        p_distance = multiprocessing.Process(target=process_sensor_distance, args=(number, q))
-        p_picar.start()
-        p_distance.start()
-        p_picar.join()
-        p_distance.join()
-    except KeyboardInterrupt:
-        stop()
+
+    p_picar = multiprocessing.Process(target=process_picar, args=(number, q))
+    p_distance = multiprocessing.Process(target=process_sensor_distance, args=(number, q))
+    p_picar.start()
+    p_distance.start()
+    p_picar.join()
+    p_distance.join()
