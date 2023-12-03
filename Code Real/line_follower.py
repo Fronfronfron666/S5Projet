7df23f9c062085170438fd5ad6a60fab65b0164b
zaccoss <zach.cossette7@gmail.com>
2023-12-03 17:28:41 -0500

    code sauceur jour 2

--- Code Real/line_follower.py
+++ Code Real/line_follower.py
@@ -52,7 +52,7 @@ def get_turn_value(line_sensor_results):
     if line_sensor_results == [False, False, False, False, False]:
 
         if previous_sensor_state == [True, False, False, False, False] or previous_sensor_state == [True, True, False, False, False]:
-            if lost_counter < 800:
+            if lost_counter < 400:
                 turn_limit = -55
                 is_spinning = True
             else:
@@ -60,7 +60,7 @@ def get_turn_value(line_sensor_results):
                 turn_limit = find_line()
 
         elif previous_sensor_state == [False, False, False, False, True] or previous_sensor_state == [False, False, False, True, True]:
-            if lost_counter < 800:
+            if lost_counter < 400:
                 turn_limit = 55
                 is_spinning = True
             else:
