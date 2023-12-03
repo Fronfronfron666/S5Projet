4ef0e32833b98b5829b911f1e629283e57e97cda
zaccoss <zach.cossette7@gmail.com>
2023-12-03 15:22:04 -0500

    code sauceur jour 2

---
+++ Code Real/m.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+from picar import front_wheels
+from picar import back_wheels
+import time
+import picar
+import mouvement as mv
+import ulrason as us
+import line_follower
+import multiprocessing
+
+picar.setup()
+fw = front_wheels.Front_Wheels(db='config')
+bw = back_wheels.Back_Wheels (db=' config')
+
+def stop():
+    bw.stop()
+    fw.turn_straight()
+
+counter = 0
+
+def process_picar(q):
+        while True:
+            mv.turn_wheels(line_follower.get_turn_value(line_follower.get_line_follower_result()))
+            #mv.turn_wheels(50)
+            mv.accelerate()
+            #mv.move()
+            mv.move_with_spin()
+            time.sleep(0.041)
+
+
+def process_sensor_distance(q):
+    while True:
+        range_value = us.get_ultrasonic_avoidance()
+        q.put(range_value)
+
+
+if __name__ == '__main__':
+    q = multiprocessing.Queue()
+    try:
+        p_picar = multiprocessing.Process(target=process_picar, args=(q))
+        p_distance = multiprocessing.Process(target=process_sensor_distance, args=(q))
+        p_picar.start()
+        p_distance.start()
+        p_picar.join()
+        p_distance.join
+    except KeyboardInterrupt:
+        stop()
--- Code Real/main.py
+++ Code Real/main.py
@@ -4,31 +4,11 @@ from picar import back_wheels
 import time
 import picar
 import mouvement as mv
-import ulrason as us
 import line_follower
 
 picar.setup()
 fw = front_wheels.Front_Wheels(db='config')
 bw = back_wheels.Back_Wheels (db=' config')
-def start_bouge():
-    print('Demarrage')
-    fw.turn_straight() # Correspond a 90
-    bw.forward()
-    bw.speed = 70
-    time.sleep(5)
-    fw.turn(0)
-    bw.backward()
-    bw.speed = 20
-    time.sleep(5)
-    fw.turn(180)
-
-    bw.forward()
-    time.sleep(5)
-    fw.turn(90)  # tout droit
-    bw.forward()
-    bw.speed = 70
-    time.sleep(5)
-    bw.stop()
 
 def stop():
     bw.stop()
@@ -36,18 +16,16 @@ def stop():
 
 counter = 0
 
+
 if __name__ == '__main__':
+
     try:
         while True:
-            print("counter: ", counter)
-
             mv.turn_wheels(line_follower.get_turn_value(line_follower.get_line_follower_result()))
-            #mv.turn_wheels(50)
+            # mv.turn_wheels(50)
             mv.accelerate()
-            #mv.move()
+            # mv.move()
             mv.move_with_spin()
-            counter += 1
-
             time.sleep(0.041)
     except KeyboardInterrupt:
         stop()
