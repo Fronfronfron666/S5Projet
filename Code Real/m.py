50bd663a469ce08b0c0aac6aa7dccd364847d4e5
zaccoss <zach.cossette7@gmail.com>
2023-12-04 13:19:07 -0500

    code sauceur jour 2

--- Code Real/m.py
+++ Code Real/m.py
@@ -29,7 +29,7 @@ def dodge():
     delai3 = delai2 + 2
 
     delai4 = delai3 + 0.7
-    delai5 = delai4 + 0.1
+    delai5 = delai4 + 0.7
     delai6 = delai5 + 0.7
 
 
@@ -47,10 +47,10 @@ def dodge():
     elif time_since_detect <= delai4:
         mv.move_frontward()
         mv.turn_wheels(-45)
+    #elif time_since_detect <= delai5:
+        #mv.move_frontward()
+        #mv.turn_wheels(0)
     elif time_since_detect <= delai5:
-        mv.move_frontward()
-        mv.turn_wheels(0)
-    elif time_since_detect <= delai6:
         mv.move_frontward()
         mv.turn_wheels(-45)
 
