9a3b941b75ae40565ee6bab5c8966e522091f4b4
zaccoss <zach.cossette7@gmail.com>
2023-12-04 20:15:34 -0500

    code sauceur jour 2

--- Code Real/line_follower.py
+++ Code Real/line_follower.py
@@ -23,7 +23,7 @@ def change_previous_sensor_result(line_sensor_results, previous_sensor_result, p
     return previous_result, previous_state
 
 
-lost_counter_threshhold = 370 - (MAX_SPEED * 2)
+lost_counter_threshhold = 350 - (MAX_SPEED * 2)
 
 
 def find_line():
