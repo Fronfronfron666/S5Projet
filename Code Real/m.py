47614181cda895f5bd242c0fb99f98b6e7fb5979
zaccoss <zach.cossette7@gmail.com>
2023-12-06 14:02:48 -0500

    code sauceur jour 2

--- Code Real/m.py
+++ Code Real/m.py
@@ -29,7 +29,7 @@ delai2 = delai1 + 2.15
 delai3 = delai2 + 0.6
 delai4 = delai3 + 3
 
-delai5 = delai4 + 0.8
+delai5 = delai4 + 1
 delai6 = delai5 + 1.3
 delai7 = delai6 + 0.1
 delai8 = delai7 + 0.8
