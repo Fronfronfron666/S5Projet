6be080cd05d484347a8a98d86e47021738f7afee
zaccoss <zach.cossette7@gmail.com>
2023-12-05 20:58:17 -0500

    code sauceur jour 2

--- Code Real/m.py
+++ Code Real/m.py
@@ -24,12 +24,12 @@ def stop():
 
 
 delai1 = 2.2
-delai2 = delai1 + 2.3
+delai2 = delai1 + 2.1
 delai3 = delai2 + 0.6
 delai4 = delai3 + 3
 delai5 = delai4 + 0.5
 delai6 = delai5 + 1.3
-delai7 = delai6 + 0
+delai7 = delai6 + 0.3
 delai8 = delai7 + 0.8
 
 def dodge():
