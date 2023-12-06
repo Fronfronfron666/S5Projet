f99d7ee5e0fd714e8c3b83665d58e86ca8e8ce0c
zaccoss <zach.cossette7@gmail.com>
2023-12-06 11:01:32 -0500

    code sauceur jour 2

--- Code Real/m.py
+++ Code Real/m.py
@@ -98,7 +98,7 @@ def process_picar(number, q):
                         detection_time = time.perf_counter()
                         mv.stop()
                         flag = True
-                    elif line_follower.last_range_value <= 13:
+                    elif line_follower.last_range_value <= 16:
                         mv.stop()
                         detection_time = time.perf_counter()
                         mv.stop()
