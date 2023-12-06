7d99021e23c1d418a6523439c3cf2f29cec04c41
zaccoss <zach.cossette7@gmail.com>
2023-12-06 11:52:39 -0500

    code sauceur jour 2

--- Code Real/m.py
+++ Code Real/m.py
@@ -25,7 +25,7 @@ def stop():
 
 if line_follower.can_spin:
     delai1 = 2.2
-    delai2 = delai1 + 2.4
+    delai2 = delai1 + 2.5
     delai3 = delai2 + 0.6
     delai4 = delai3 + 2.5
     delai5 = delai4 + 0.5
