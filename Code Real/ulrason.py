ba5b3949eecfce6307b97aebce726801c5178e45
Fronfronfron666 <pelf1504@usherbrooke.ca>
2023-12-08 15:43:29 -0500

    Added comments in ultrason.py

--- Code Real/ulrason.py
+++ Code Real/ulrason.py
@@ -2,7 +2,11 @@ from SunFounder_Ultrasonic_Avoidance import Ultrasonic_Avoidance
 
 ultrasonic_avoidance = Ultrasonic_Avoidance.Ultrasonic_Avoidance(20)
 
+"""
+Entrée: N/A
+Sortie: Distance detecte par le capteur ultrasonique
+Fonction: Donne la distance detecte par le capteur ultrasonique
+"""
 def get_ultrasonic_avoidance():
         return ultrasonic_avoidance.get_distance()
-def less_than(value):
-        return ultrasonic_avoidance.less_than(value)
+
